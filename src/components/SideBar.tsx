28cb69783df7de08a5030529882277fcd81cfe29
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-07-30 18:18:14 +0300

    Create hero & footer, create product detail page

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -10,7 +10,7 @@ const SideBar = () => {
   // get toggle with context api to toggle sidebar with style
   const { isOpen, toggleSidebar } = useContext(SlidebarContext);
 
-  const { cart, clearCart, totalPrice } = useContext(CartContext);
+  const { cart, clearCart, totalPrice, itemamount } = useContext(CartContext);
   return (
     <div
       // toggle sidebar with style, if isOpen is true, add class right-0, else add class -right-full
@@ -19,7 +19,9 @@ const SideBar = () => {
       } bg-white w-full h-full fixed top-0 shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-500 ease-in-out z-20 px-4 lg:px-[35px]`}
     >
       <div className="flex justify-between items-center py-6 border-b">
-        <div className="uppercase text-sm font-semibold">Shopping Cart (0)</div>
+        <div className="uppercase text-sm font-semibold">
+          Shopping Cart ({itemamount})
+        </div>
         {/* icon */}
         <div
           onClick={toggleSidebar}
@@ -28,7 +30,7 @@ const SideBar = () => {
           <IoMdArrowForward onClick={toggleSidebar} className="text-2xl" />
         </div>
       </div>
-      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
+      <div className="flex flex-col gap-y-2 h-[312px] lg:h-[380px] xl:h-[600px] overflow-y-auto overflow-x-hidden border-b">
         {cart.map((product) => {
           return <CartItem item={product} key={product.id} />;
         })}
@@ -41,10 +43,25 @@ const SideBar = () => {
             {parseFloat(totalPrice).toFixed(2)}
           </div>
           {/* clear cart icon */}
-          <div className="cursor-pointer" onClick={clearCart}>
-            <FiTrash2 className="py-4 text-white w-12 h-12 hover:text-red-500 transition flex justify-center items-center text-xl" />
+          <div
+            className="cursor-pointer py-4 w-12 h-12 text-black hover:text-red-500 transition flex justify-center items-center text-xl"
+            onClick={clearCart}
+          >
+            <FiTrash2 />
           </div>
         </div>
+        <Link
+          to={"/"}
+          className="bg-gray-200 flex py-4 justify-center items-center text-primary w-full font-medium"
+        >
+          View Cart
+        </Link>
+        <Link
+          to={"/"}
+          className="bg-primary flex py-4 justify-center items-center text-white w-full font-medium"
+        >
+          Checkout
+        </Link>
       </div>
     </div>
   );
